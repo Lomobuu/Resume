64725eeb60a7a77a3213dc3ab9f5d263167ee4e1
GitHubFozzen <simen.fossen@lyse.net>
2022-10-04 15:01:22 +0200

    add html to site

--- src/App.js
+++ src/App.js
@@ -3,20 +3,38 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Simen Fossen</a>
+          </div>
+          <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#About">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#Skills">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#Socials">Socials</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="#Contact">Contact me!</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-warning" href="Https://www.fozzen.com">Climbing Hobby</a>
+          </li>
+        </ul>
+        </nav>
+
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
